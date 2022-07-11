163e9b9c149560cefa84444a67404241fa426669
DF490 <diego.mc09@outlook.com>
2022-07-11 17:16:46 -0500

    Proyecto creado

---
+++ src/Tema1/TipoDeDatos.java
@@ -0,0 +1,26 @@
+package Tema1;
+
+public class TipoDeDatos {
+
+    public static void main (String[] args){
+
+        String texto = "Tipos de datos:";
+        int num1 = 5;
+        long num2 = 2;
+        double num3 = 5.2;
+        boolean verdadero = true;
+        byte num4 = 1;
+        short num5 = 45;
+
+
+        System.out.println(texto);
+        System.out.println("El  entero es :" + num1);
+        System.out.println("El número long es:" + num2);
+        System.out.println("El número double es:" + num3);
+        System.out.println("El número byte es:" + num4);
+        System.out.println("El número short es:" + num5);
+        System.out.printf("El valor Boolean es:" + verdadero);
+
+    }
+
+}
