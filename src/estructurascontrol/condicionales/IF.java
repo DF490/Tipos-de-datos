daec419d07536a26d2bbacbe29b7e5ec1c6eeadb
DF490 <diego.mc09@outlook.com>
2022-07-14 17:16:17 -0500

    If Else

--- src/estructurascontrol/condicionales/IF.java
+++ src/estructurascontrol/condicionales/IF.java
@@ -1,4 +1,15 @@
 package estructurascontrol.condicionales;
 
 public class IF {
+
+    public static void main(String[] args){
+
+        int edad = 17;
+
+        if (edad <= 17){
+
+            System.out.printf("Es menor de edad!");
+        }
+
+    }
 }
---
+++ src/estructurascontrol/condicionales/IfElse.java
@@ -0,0 +1,17 @@
+package estructurascontrol.condicionales;
+
+public class IfElse {
+
+    public static void main (String[] args){
+
+        int edad = 13;
+
+        if (edad <= 17){
+            System.out.printf("Es menor de edad!!");
+        }
+        else{
+            System.out.printf("¡Es mayor de edad!");
+        }
+    }
+
+}
