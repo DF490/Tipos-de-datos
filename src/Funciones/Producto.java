8e8eb17cd5f5c64570662081589baebd6caec717
DF490 <diego.mc09@outlook.com>
2022-07-14 16:34:12 -0500

    Pruducto.java

---
+++ src/Funciones/Producto.java
@@ -0,0 +1,16 @@
+package Funciones;
+
+public class Producto {
+
+    public static void main(String[] args){
+         int resultado = operacion(700);
+        System.out.println(resultado);
+    }
+
+    static int operacion(int precio){
+        int iva = 19;
+        return (precio * iva /100);
+    }
+
+
+}
