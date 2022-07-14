0a875c5b500a129d2e7353e89ee2d035abe4a6b0
DF490 <diego.mc09@outlook.com>
2022-07-14 16:47:35 -0500

    Detalles visuales

--- src/Funciones/Producto.java
+++ src/Funciones/Producto.java
@@ -3,13 +3,14 @@ package Funciones;
 public class Producto {
 
     public static void main(String[] args){
-         int resultado = operacion(700);
+        int resultado = operacion(700);
         System.out.println(resultado);
     }
 
     static int operacion(int precio){
         int iva = 19;
         int res = (precio * iva /100) + precio;
+        System.out.println("Precio Producto: " + precio + "\n" + "Valor del iva: " + iva + "\n" + "Total iva incluido: " + res );
         return res;
     }
 
